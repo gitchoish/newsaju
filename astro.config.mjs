46fbbb3eb928d4ca3567ef885d6bad143cea7f39
gitchoish <151187722+gitchoish@users.noreply.github.com>
2026-01-16 22:18:14 +0900

    ㅁㄴㅇㄹ

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://newsaju.pages.dev',
   output: 'static',
   integrations: [
     tailwind(),
-    mdx()
+    mdx(),
+    sitemap()
   ],
   markdown: {
     shikiConfig: {
